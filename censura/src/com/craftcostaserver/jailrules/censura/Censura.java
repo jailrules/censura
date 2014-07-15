fc35df5206cbebeea590287ead82f8cd55e0b0dc
Jailrules <Mercedes@Mercedes-PC>
2014-07-16 01:48:42 +0200

    added config file

--- censura/src/com/craftcostaserver/jailrules/censura/Censura.java
+++ censura/src/com/craftcostaserver/jailrules/censura/Censura.java
@@ -173,7 +173,7 @@ public class Censura extends JavaPlugin{
 				for(int i=2;i<args.length;i++){
 					aux+=args[i]+" ";
 				}
-				if(player.performCommand("tempmute "+ args[0] + " " + args[1])){
+				if(player.performCommand("tempmute "+ args[0] + " " + args[1] + " " + aux)){
 					player.sendMessage(ChatColor.RED + "Muteaste temporalmente a: "+ ChatColor.WHITE+ args[0]);
 					player.sendMessage(ChatColor.RED + "Duracion: "+ ChatColor.WHITE+ args[1]);
 					player.sendMessage(ChatColor.RED + "Razon: "+ ChatColor.WHITE +aux);
@@ -223,6 +223,37 @@ public class Censura extends JavaPlugin{
 			}
 		}
 		
+		if(commandLabel.equalsIgnoreCase("cjailmute")){
+			if(args.length<4){
+				player.sendMessage(ChatColor.RED + "[Censura] Numero de argumentos incorrecto!!");
+				return false;
+			}else{
+				if(player.performCommand("jail "+ args[0] + " " + args[1] + " " +args[2])){
+					for(int i=3;i<args.length;i++){
+						aux+=args[i]+" ";
+					}
+					player.sendMessage(ChatColor.RED + "Encarcelando a: "+ ChatColor.WHITE+ args[0]);
+					player.sendMessage(ChatColor.RED + "Celda: "+ ChatColor.WHITE +args[1]);
+					player.sendMessage(ChatColor.RED + "Duracion: "+ ChatColor.WHITE +args[2]);
+					player.sendMessage(ChatColor.RED + "Razon: "+ ChatColor.WHITE +aux);
+				}else{
+					player.sendMessage(ChatColor.RED + "[Censura] Argumentos incorrectos!! Revisa los datos introducidos.");
+					return false;
+				}
+				if(player.performCommand("tempmute "+ args[0] + " " +args[2]+ " " + aux)){
+					player.sendMessage(ChatColor.RED + "Encarcelando a: "+ ChatColor.WHITE+ args[0]);
+					player.sendMessage(ChatColor.RED + "Celda: "+ ChatColor.WHITE +args[1]);
+					player.sendMessage(ChatColor.RED + "Duracion: "+ ChatColor.WHITE +args[2]);
+					player.sendMessage(ChatColor.RED + "Razon: "+ ChatColor.WHITE +aux);
+				}else{
+					player.sendMessage(ChatColor.RED + "[Censura] Argumentos incorrectos!! Revisa los datos introducidos.");
+					return false;
+
+				}
+				return true;
+			}
+		}
+		
 		if(commandLabel.equalsIgnoreCase("cphistory")){
 			if(args.length<2){
 				player.sendMessage(ChatColor.RED + "[Censura] Numero de argumentos incorrecto!!");
@@ -233,6 +264,7 @@ public class Censura extends JavaPlugin{
 				return true;
 			}
 		}
+		
 		return false;
 	}
 }
